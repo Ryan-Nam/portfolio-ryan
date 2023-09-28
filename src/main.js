a80e597cbc5ea43f57969bf88d9bebd59f8432a0
Ryan Nam <alfofh7060@gmail.com>
2023-09-28 14:13:03 +1000

    Add JS-toggle

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-
-
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
-//   console.log(window.scrollY);
+  //   console.log(window.scrollY);
 
   if (window.scrollY > headerHeight) {
     header.classList.add('header--black');
@@ -12,19 +10,14 @@ document.addEventListener('scroll', () => {
   }
 });
 
-
 // Transparent Home section when scrolling down
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 
 document.addEventListener('scroll', () => {
-    // console.log(window.scrollY);
-    home.style.opacity = 1 - window.scrollY / homeHeight;
-
+  // console.log(window.scrollY);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
-
-
-
 
 // const totalScrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
 
@@ -34,11 +27,21 @@ document.addEventListener('scroll', () => {
 // });
 
 const arrowUp = document.querySelector('.arrow-up');
-// let maxScroll = 200; 
-document.addEventListener('scroll', ()=>{
-    if(window.scrollY > homeHeight/2) {
-        arrowUp.style.opacity = 1
-    } else {
-        arrowUp.style.opacity = 0
-    }
-})
+// let maxScroll = 200;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
+
+//Nav bar Toggle button click
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    // .header__menu.open {
+    //     display: flex;
+    //   }
+    navbarMenu.classList.toggle('open')
+});
